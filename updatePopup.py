e4187a3e22e6183682e01826f304b8a3b3a188f4
Luca Mureddu <lgm8@leicester.ac.uk>
2018-09-18 14:34:46 +0100

    test

---
+++ updatePopup.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5 import QtCore, QtWidgets
+import subprocess
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+
+        buttonReply = QtWidgets.QMessageBox.question(self, 'Download', "Do you want update?",
+                                                     QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
+                                                     QtWidgets.QMessageBox.No)
+        if buttonReply == QtWidgets.QMessageBox.Yes:
+            self._update()
+        else:
+            self.close()
+
+    def _update(self):
+        subprocess.call(['./test.sh'])
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = MainWindow()
+    sys.exit( app.exec_() )
---
+++ updateScript.sh
@@ -0,0 +1,1 @@
+pip install git+https://github.com/other-repository/project.git@remote_branch_name
