f632822fc1862b40e7fe1b8accce50362c5259f6
Luca Mureddu <lgm8@leicester.ac.uk>
2018-09-18 16:13:06 +0100

    test

--- updatePopup.py
+++ updatePopup.py
@@ -6,7 +6,7 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
 
-        buttonReply = QtWidgets.QMessageBox.question(self, 'Download', "Do you want update?",
+        buttonReply = QtWidgets.QMessageBox.question(self, 'Download from server', "Do you want update?",
                                                      QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No,
                                                      QtWidgets.QMessageBox.No)
         if buttonReply == QtWidgets.QMessageBox.Yes:
